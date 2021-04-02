38e9361437629ce618713a8bbdbaaac657c8fbfe
Omurbekova-Ajar <oajara@mail.ru>
2021-04-02 21:02:46 +0600

    comment

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Post, Tag
+from django.views.generic import View
+from .utils import ObjectDetailMixin
+from .forms import TagForm
+
+# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tags_create.html', context={'form': form})
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
